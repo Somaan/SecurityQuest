5a6932360ea14cd58baa615fddb967ef1972b68a
somaan02 <xre20jgu@uea.ac.uk>
2025-04-01 14:11:41 +0100

    Using Heroku to store website online for user testing accessability

--- vite/somaans-project/src/backend/db.js
+++ vite/somaans-project/src/backend/db.js
@@ -3,9 +3,8 @@ const { Pool } = require('pg');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: isProduction ? { rejectUnauthorized: false} : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
-
 
 module.exports = pool;
